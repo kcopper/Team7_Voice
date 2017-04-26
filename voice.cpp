ededede79835b027bf60c8e22c45be6b23cba353
patrick donavan <donava.2Wright.edu>
2017-04-26 19:53:41 -0400

    create and test user

--- voice.cpp
+++ voice.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main() {
     VoiceIt myVoiceIt("908e399927154baab125a5c2c9dc36af");
 	//test change.
+	String reponce = myVoiceIt.createUser("donavan.2@wright.edu","Pokemon1","Patrick","Donavan","513-484-5459");
+    String reponce = myVoiceIt.createEnrollment("donavan.2@wright.edu","Pokemon1","/home/user/username/voicePrint.wav","en-US");
+    String reponce = myVoiceIt.authentication("donavan.2@wright.edu","Pokemon1","/home/user/username/voicePrint.wav","90","en-US");
+
     return 0;
 }
