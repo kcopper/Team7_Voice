6485d5ae1e0f84c18d6bffba76e2315cf8c04b39
don <don@don-VirtualBox>
2017-04-26 16:47:52 -0400

    tests.cpp scaffold built for VoiceIt.h

---
+++ tests.cpp
@@ -0,0 +1,32 @@
+#include <assert>
+#include "VoiceIt.h"
+
+using namespace std;
+
+//Unit tests
+void test_Obj_SHA256(){
+	try{
+		SHA256* test = SHA256();
+	}catch(exception e){
+		cout<<"Failed to create SHA256 carrier object."<<"\n";
+	}
+	cout<<"SHA256 carrier object created successfully."<<"\n";
+	//Object method tests
+
+}
+void test_Obj_VoiceIt
+void test_getUser(){}
+void test_setUser(){}
+void test_createUser(){}
+void test_deleteUser(){}
+void test_createEnrollment(){}
+void test_createEnrollmentOverloaded(){}
+void test_createEnrollmentByWavUrl(){}
+void test_createEnrollmentByWavUrlOverloaded(){}
+void test_getEnrollment(){}
+void test_deleteEnrollment(){}
+void test_authentication(){}
+void test_authenticationOverloaded(){}
+void test_authenticationByWavUrl(){}
+void test_authenticationByWavUrlOverloaded(){}
+
