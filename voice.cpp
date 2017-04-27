542f52999439a8ec5c13c1f8ba4162549d7e43c3
patrick donavan <donava.2Wright.edu>
2017-04-26 20:03:31 -0400

    removed cell

--- voice.cpp
+++ voice.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     VoiceIt myVoiceIt("908e399927154baab125a5c2c9dc36af");
 	//test change.
-	string createResponse = myVoiceIt.createUser("donavan.2@wright.edu","Pokemon1","Patrick","Donavan","513-484-5459");
+	string createResponse = myVoiceIt.createUser("donavan.2@wright.edu","Pokemon1","Patrick","Donavan");
     string enrollResponse = myVoiceIt.createEnrollment("donavan.2@wright.edu","Pokemon1","/home/user/username/voicePrint.wav","en-US");
     string authResponse = myVoiceIt.authentication("donavan.2@wright.edu","Pokemon1","/home/user/username/voicePrint.wav","90","en-US");
 
