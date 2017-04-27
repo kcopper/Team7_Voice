58ebeef22b904c27ae8055c358a07b180df8bbc8
don <don@don-VirtualBox>
2017-04-27 13:13:28 -0400

    Working success tests. Filling in errors now.

--- tests.cpp
+++ tests.cpp
@@ -38,19 +38,30 @@ void test_Obj_VoiceIt(){
 	result = myVoiceIt.setUser("test@email.com", "testing", "Test", "McTester");
 	cout<<"Set User Test"<<result<<endl;
 	//createEnrollment
-	result = myVoiceIt.createEnrollment("test@email.com", "testing", "rec_4s.wav", "en-US");
-	cout<<"Create Enrollment Test"<<result<<endl;
+	cout<<"Authentication requires 3 valid enrollments"<<endl;
+	result = myVoiceIt.createEnrollment("test@email.com", "testing", "TestPhraseEnroll1.wav", "en-US");
+	cout<<"Create Enrollment1 Test Success. "<<result<<endl;
+	result = myVoiceIt.createEnrollment("test@email.com", "testing", "TestPhraseEnroll2.wav", "en-US");
+	cout<<"Create Enrollment2 Test Success. "<<result<<endl;
+	result = myVoiceIt.createEnrollment("test@email.com", "testing", "TestPhraseEnroll3.wav", "en-US");
+	cout<<"Create Enrollment3 Test Success. "<<result<<endl;
 	//getEnrollment
 	result = myVoiceIt.getEnrollments("test@email.com", "testing");
-	cout<<"Get Enrollment Test"<<result<<endl;
-	//deleteEnrollment
+	cout<<"Get Enrollment Test Success. "<<result<<endl;
 	size_t start = result.find("[") + 1;
-	size_t end = result.find(",");
-	string num = result.substr(start, end-start);
-	cout<<num<<endl;
-	//myVoiceIt.deleteEnrollment("email2@email.com", "new_pass");
+	size_t end = 7;
+	string num = result.substr(start, end);
 	//authentication
-	//myVoiceIt.authentication();
+	result = myVoiceIt.authentication("test@email.com", "testing", "TestPhraseAuth.wav", "90", "en-US");
+	cout<<"Authentication Test Success. Confidence 90 "<<result<<endl;
+	result = myVoiceIt.authentication("test@email.com", "testing", "TestPhraseLow.wav", "90", "en-US");
+	cout<<"Authentication Test Fail, too low. Confidence 90"<<result<<endl;
+	result = myVoiceIt.authentication("test@email.com", "testing", "TestPhraseHigh.wav", "90", "en-US");
+	cout<<"Authentication Test Fail, too high. Confidence 90"<<result<<endl;
+	//deleteEnrollment
+
+	result = myVoiceIt.deleteEnrollment("test@email.com", "testing", num);
+	cout<<"Delete Enrollment Test Success. "<<result<<endl;
 	//deleteUser
 	result = myVoiceIt.deleteUser("test@email.com", "testing");
 	cout<<"Delete User Test"<<result<<endl;
